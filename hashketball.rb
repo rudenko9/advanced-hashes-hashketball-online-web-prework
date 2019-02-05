0ba01da3b1f163bd658d2cc89f1192de07c06530
Nataliya R <n.rudenko@gmail.com>
2019-02-05 22:33:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -58,7 +58,10 @@ def game_hash
            }]
        },
     :away => {
-      
+      :team_name => "Charlotte Hornets", 
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {
     }
   }
   
