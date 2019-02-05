f1180fbc18ebf5449ecd10ad0370440639f4e44c
Nataliya R <n.rudenko@gmail.com>
2019-02-05 22:15:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,7 +16,16 @@ def game_hash
           :blocks => "1",
           :slum_dunks => "1"
         },
-        {
+        {:player_name => "Reggie Evans",
+          :number => "30",
+          :shoe => "14",
+          :points => "12",
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "12",
+          :blocks => "12",
+          :slum_dunks => "7"},
+          {
           
         }]
       
