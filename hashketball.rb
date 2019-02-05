8fe0bd519e1ce9f5465537ab84de3cb2169473cb
Nataliya R <n.rudenko@gmail.com>
2019-02-05 22:35:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,7 +61,17 @@ def game_hash
       :team_name => "Charlotte Hornets", 
       :colors => ["Turquoise", "Purple"],
       :players => [
-        {
+        { :player_name => "Jeff Adrien",
+          :number => "4",
+          :shoe => "18",
+          :points => "10",
+          :rebounds => "1",
+          :assists => "1",
+          :steals => "2",
+          :blocks => "7",
+          :slum_dunks => "2"},
+          
+          {
     }
   }
   
