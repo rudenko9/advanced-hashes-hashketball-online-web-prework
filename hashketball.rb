18e2e77c5ccc99e772dc2958e2637bafa804dbad
Nataliya R <n.rudenko@gmail.com>
2019-02-05 22:06:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,6 +6,17 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [
         {
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "3",
+          :blocks => "1",
+          :slum_dunks => "1"
+        },
+        {
           
         }]
       
