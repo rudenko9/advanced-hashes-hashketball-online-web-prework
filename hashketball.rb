41cb7b48c554bda4af43079949b3684dcc8c0168
Nataliya R <n.rudenko@gmail.com>
2019-02-06 01:14:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ def points_of_player(player_name, game)
   game.each do |team, team_hash|
     team_hash[:players].each do |player, player_hash|
       if player_hash[:name] == player_name
-        return player_hash[:stats][:points]
+        return player_hash[:points]
       end
     end
   end
