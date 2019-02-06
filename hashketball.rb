4abb1c594ae77a31809a12c127bb4f61ebaf9795
Nataliya R <n.rudenko@gmail.com>
2019-02-06 00:06:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,8 +115,9 @@ def game_hash
   }
 end
 
-def num_points_scored
-  away_players = 
+def num_points_scored(name)
+  find_player = players.find {|player| player.fetch (:player_name) == name}
+  find_player.fetch(:points)
   
   
 
